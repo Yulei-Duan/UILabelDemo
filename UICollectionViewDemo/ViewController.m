16d1938c0ff6ecaec7b50a9a6f24fb88879726b3
Leviduan <duan@leviduan-2.local>
2017-02-16 17:37:14 +0800

    Initial Commit

---
+++ UICollectionViewDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UICollectionViewDemo
+//
+//  Created by Leviduan on 2017/2/16.
+//  Copyright © 2017年 Leviduan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
